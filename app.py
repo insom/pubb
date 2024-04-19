69e7bceaeb7be8c78d7f8284ad482434297f3d14
Aaron Brady <aaron@insom.me.uk>
2024-04-19 04:31:34 +0000

    Starting HTTP signatures

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import time, json, base64
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -75,8 +76,25 @@ def followers():
 @app.route("/inbox", methods=["POST"])
 def inbox():
     j = request.get_json(force=True)
-    import time
-    import json
     f = open("%d.json" % time.time(), 'w')
     f.write(json.dumps(j))
     return ('', 201, {})
+
+def sign(body, hostname, path):
+    from email.utils import formatdate
+    from Crypto.Hash import SHA256
+    from Crypto.Signature import pkcs1_15
+    from Crypto.PublicKey import RSA
+
+    now = formatdate(usegmt=True)
+    pk = RSA.import_key(open('private.key').read())
+    post_hash = SHA256.new(body.encode('u8')).digest()
+
+    headers = ("(request-target): post " + path + "\n" +
+                 "host: " + hostname + "\n" +
+                 "date: " + now + "\n" +
+                 "digest: SHA-256=" + base64.b64encode(post_hash).decode())
+    signature = base64.b64encode(pkcs1_15.new(pk).sign(SHA256.new(headers.encode('u8')))).decode() # such transcode, wow
+    keyId = "https://%(HOSTNAME)s/u/%(USERNAME)s#main-key" % app.config
+    sig_header = 'keyId="%s", algorithm="rsa-sha256", headers="(request-target) host date digest", signature="%s"' % (keyId, signature)
+    print(sig_header)
